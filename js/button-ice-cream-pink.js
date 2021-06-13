b345f3975f3adb72d1b8e5901689af9969eea1b2
apfdamascena <apfd@cin.ufpe.br>
2021-06-13 02:21:09 -0300

    pink button done

--- js/button-ice-cream-pink.js
+++ js/button-ice-cream-pink.js
@@ -1,8 +1,8 @@
-const blueButton = document.getElementById('ice-cream-blue');
+const pinkButton = document.getElementById('ice-cream-pink');
 
-blueButton.addEventListener('click', () => {
+pinkButton.addEventListener('click', () => {
     const header = document.getElementsByTagName('header')[0];
     const footer = document.getElementsByTagName('footer')[0];
-    header.style.cssText = "background: linear-gradient(180deg, var(--BLUE-BACKGROUND) 44.17%, var(--SECOND-HEADER-COLOR) 100%);"
-    footer.style.cssText = "background: var(--BLUE-BACKGROUND); color: var(--MEDIUM-TEXT)"
+    header.style.cssText = "background: linear-gradient(180deg, var(--FIRST-HEADER-COLOR) 44.17%, var(--SECOND-HEADER-COLOR) 100%);"
+    footer.style.cssText = "background: var(--FOOTER-COLOR); color: var(--TEXT)"
 });
